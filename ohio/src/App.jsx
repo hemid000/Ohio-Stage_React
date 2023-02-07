3403f78fccfbf452275b9719aab8bed90de61db9
hemid000 <hmid.yusifli04@mail.ru>
2023-02-07 18:18:13 +0400

    about us (design creators) section 1

---
+++ ohio/src/App.jsx
@@ -0,0 +1,11 @@
+import Offer from "./Components/Pages/Home/Offer/Offer";
+
+function App() {
+  return (
+    <main>
+      <Offer />
+    </main>
+  );
+}
+
+export default App;
--- ohio/src/index.js
+++ ohio/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Layout from "./Layout/Layout";
+import Layout from "../src/Components/Layout/Layout";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
