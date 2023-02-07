52488c326309295169f418846a6fe070c9f1e0a2
hemid000 <hmid.yusifli04@mail.ru>
2023-02-07 22:58:25 +0400

    Product imgs and product img information in modal

--- ohio/src/App.jsx
+++ ohio/src/App.jsx
@@ -1,10 +1,12 @@
 import Offer from "./Components/Pages/Home/Offer/Offer";
 import Services from "./Components/Pages/Home/Services/Services";
+import ProductImg from "./Components/Pages/Home/ProductImg/ProductImg";
 function App() {
   return (
     <main>
       <Offer />
       <Services />
+      <ProductImg />
     </main>
   );
 }
