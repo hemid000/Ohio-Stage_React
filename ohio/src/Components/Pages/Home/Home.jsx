4bd51cf8e45024e5260e12c5ad793b025a43bfef
hemid000 <hmid.yusifli04@mail.ru>
2023-02-08 17:45:05 +0400

    added

--- ohio/src/Components/Pages/Home/Home.jsx
+++ ohio/src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Offer from "../Home/Offer/Offer";
 import Services from "../Home/Services/Services";
 import ProductImg from "../Home/ProductImg/ProductImg";
+import Capabilities from "./Capabilities/Capabilities";
 function Home() {
   return (
     <main>
       <Offer />
       <Services />
       <ProductImg />
+      <Capabilities />
     </main>
   );
 }
