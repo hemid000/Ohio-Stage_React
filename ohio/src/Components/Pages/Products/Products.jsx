f9c7b7ebf7ed8dbc95e0ad5bda1f7c91ed63284c
hemid000 <hmid.yusifli04@mail.ru>
2023-02-19 15:11:48 +0400

    Related Products ws

--- ohio/src/Components/Pages/Products/Products.jsx
+++ ohio/src/Components/Pages/Products/Products.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductsInfo from "./StandartProducts/ProductsInfo";
 import ProductReview from "./ProductReview/ProductReview";
+import Related from "./RelatedProducts/Related";
 const Products = () => {
   return (
     <main>
       <ProductsInfo />
       <ProductReview />
+      <Related />
     </main>
   );
 };
